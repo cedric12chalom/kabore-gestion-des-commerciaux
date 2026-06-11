68b23d79f125e2273c51abc7f91920aba211ba33
cedric12chalom <cedric.kabore@saintjeaningenieur.org>
2026-06-11 16:49:10 +0100

    final result

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,6 +1,3 @@
-"""
-ASGI config for GeoCommerce Pro - Support WebSocket via Channels
-"""
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -8,14 +5,9 @@ from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# Importer les routes WebSocket après le setup Django
-django_asgi_app = get_asgi_application()
-
-from apps.gps.routing import websocket_urlpatterns
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+    URLRouter(websocket_urlpatterns)
     ),
 })
