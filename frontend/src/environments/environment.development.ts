f113c8bd466731d3550069525915ddfa1206ef8f
cedric12chalom <cedric.kabore@saintjeaningenieur.org>
2026-06-14 16:08:50 +0100

    fix cors et url api

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  production: false,  // ← false en dev
-  apiUrl: 'http://127.0.0.1:8000/api/v1',      // ← localhost
-  authUrl: 'http://127.0.0.1:8000/api/v1/auth', // ← localhost + /v1
-  wsUrl: 'ws://127.0.0.1:8000/ws',              // ← localhost
+  production: true,
+  apiUrl: (globalThis as any).__env?.apiUrl ?? 'https://kabore-gestion-des-commerciaux-3.onrender.com/api/v1',
+  authUrl: (globalThis as any).__env?.authUrl ?? 'https://kabore-gestion-des-commerciaux-3.onrender.com/api/v1/auth',
+  wsUrl: (globalThis as any).__env?.wsUrl ?? 'wss://kabore-gestion-des-commerciaux-3.onrender.com/ws/gps/',
   appName: 'GeoCommerce Pro',
   version: '1.0.0',
   gpsUpdateInterval: 30000,
